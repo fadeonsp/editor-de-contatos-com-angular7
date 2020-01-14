71da61eef1d4fc4f30b312b8cef5269d2bfdb58a
eduardo <edfadeon@gmail.com>
2020-01-14 18:27:15 -0300

    um app de contatos com angular 7

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { ContatoService } from './../services/contato.service';
+import { Contato } from './../models/contato.model';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  contato:Contato;
+  contatos: Contato[];
+
+  constructor(private contatoService: ContatoService) { }
+
+  ngOnInit() {
+    this.contato = new Contato();
+    this.contatos = this.contatoService.getAll()
+  }
+
+  edit(contato: Contato) {
+    this.contato = contato;
+  }
+  delete(contato: Contato) {
+    let index = this.contatos.map((item) => item.id).indexOf(contato.id)
+    this.contatoService.delete(contato)
+    this.contatos.splice(index, 1)
+  }
+
+  saveContato(form : NgForm){
+    this.contato = form.value;
+    this.contato.id = (this.contato.id) ? this.contato.id : new Date().getTime().toString();
+    this.contatos = this.contatoService.getAll()
+    form.resetForm();
+  }
+
+}
---
+++ src/app/services/contato.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { LocalStorageService } from 'angular-2-local-storage';
+import { Contato } from '../models/contato.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContatoService {
+
+  constructor(private localStorage : LocalStorageService) { }
+
+  delete(contato: Contato) {
+    this.localStorage.remove(contato.id);
+  }
+  save(contato: Contato) {
+    this.localStorage.set(contato.id, contato);
+  }
+  // método para recuperar os contatos
+  getContato(id: string): Contato{
+    return this.localStorage.get(id);
+  }
+  // recupera todos os contatos que estão sendo salvos
+  getAll(): Contato[]{
+    return this.localStorage.keys().map(id => this.getContato(id));
+  }
+}
